8770117ef39b6e3196e082aa38050e51c22117ab
ishant1809 <ishantbhandari1809@gmail.com>
2025-04-16 23:51:43 +0530

    Game features added

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ var game = new Chess()
 var $status = $('#status')
 var $fen = $('#fen')
 var $pgn = $('#pgn')
+let c_player = null;
 
 function onDragStart(source, piece, position, orientation) {
+  if (game.turn() != c_player) {
+    return false;
+  } 
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
 
@@ -25,10 +29,28 @@ function onDrop(source, target) {
   })
 
   // illegal move
-  if (move === null) return 'snapback'
-
-  updateStatus()
+  if (move === null) return 'snapback';
+  socket.emit('sync_state',game.fen(),game.turn())
+  updateStatus();
 }
+
+
+function onChange (){
+  if(game.game_over()){
+    if(game.in_checkmate()){
+      const winner=game.turn()=== 'b' ? 'white':'black';
+      socket.emit("game_over",winner)
+    }
+  }
+
+}
+
+
+
+
+
+
+
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
@@ -74,8 +96,9 @@ var config = {
   position: 'start',
   onDragStart: onDragStart,
   onDrop: onDrop,
-  onSnapEnd: onSnapEnd
-}
+  onSnapEnd: onSnapEnd,
+  onChange: onChange,
+};
 board = Chessboard('myBoard', config)
 
 
@@ -85,7 +108,12 @@ updateStatus()
 
 function handleButtonClick(event) {
   const timer = Number(event.target.getAttribute('data-time'));
-  alert(timer);
+  socket.emit('want_to_play', timer);
+  $('#main-element').hide();
+  $('#waiting_text_p').show();
+
+
+
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -100,6 +128,32 @@ const socket = io('http://localhost:3000');
 socket.on("total_players_count_change", function (totalPlayersCount) {
   $("#total_players").html("Total Players: " + totalPlayersCount)
 });
-console.log(socket);
+
+socket.on("match_made", (color) => {
+  c_player = color;
+  $('#main-element').show();
+  $('#waiting_text_p').hide();
+  $("#buttonsParent").html('<p id="yaps">You are playing as ' + (color === 'b' ? 'BLACK' : 'WHITE') + '</p>');
+  game.reset();
+  board.clear();
+  board.start();
+  board.orientation(color === 'b' ? 'black' : 'white');
 
 
+});
+
+socket.on('sync_state_from_server', function(fen, turn){
+  game.load(fen);
+  game.setTurn(turn);
+  board.position(fen);
+  onChange(); // <- THIS IS THE FIX!
+});
+
+
+
+socket.on("game_over_from_server",function(winner){
+
+  const message=winner=== c_player ? "YOU WON" : "YOU LOST";
+  alert(message);
+  window.location.reload();
+})
--- socket.js
+++ socket.js
@@ -10,23 +10,96 @@ const io = new Server(httpServer, {
 });
 let totalPlayers = 0;
 let players = {};
+let waiting = {
+    10: [],
+    15: [],
+    20: [],
+};
+
+let matches = {
+    10: [],
+    15: [],
+    20: [],
+};
+
+
+
+
+
+
+
+function removeSocketFromWaitingPeriod(socket) {
+    const foreachLoop = [10, 15, 20];
+    foreachLoop.forEach((element) => {
+        const index = waiting[element].indexOf(socket);
+        if (index > -1) {
+            console.log(waiting[element].splice(index, 1));
+        }
+    });
+}
+
 
 function fireTotalPlayers() {
     io.emit('total_players_count_change', totalPlayers);
 }
 
 function FireonDisConnect(socket) {
+    removeSocketFromWaitingPeriod(socket.id);
+
     totalPlayers--;
     fireTotalPlayers();
 }
 
+function initialSetupMatch(opponentId, socketId) {
+    players[opponentId].emit("match_made", "w");
+    players[socketId].emit("match_made", "b");
+
+    players[opponentId].on("sync_state", function (fen, turn) {
+        players[socketId].emit("sync_state_from_server", fen, turn);
+    });
+    players[socketId].on("sync_state", function (fen, turn) {
+        players[opponentId].emit("sync_state_from_server", fen, turn);
+    });
+    players[opponentId].on("game_over", function (winner) {
+        players[socketId].emit("game_over_from_server", winner);
+    });
+    players[socketId].on("game_over", function (winner) {
+        players[opponentId].emit("game_over_from_server", winner);
+    });
+
+
+}
+
+function HandlePlayRequest(socket, time) {
+    if (waiting[time].length > 0) {
+        const opponentId = waiting[time].splice(0, 1)[0]
+        matches[time].push({
+            [opponentId]: socket.id
+        });
+        initialSetupMatch(opponentId, socket.id)
+        return;
+    }
+
+    if (!waiting[time].includes(socket.id)) {
+        waiting
+        [time].push(socket.id);
+    }
+}
+
+
+
 function FireOnConnected(socket) {
+    socket.on("want_to_play", function (timer) {
+        console.log(timer);
+        HandlePlayRequest(socket, timer);
+        console.log(waiting);
+    });
     totalPlayers++;
     fireTotalPlayers();
 }
 
 io.on("connection", (socket) => {
-    players[socket.id] = socket
+    players[socket.id] = socket;
     FireOnConnected(socket)
 
     socket.on('disconnect', () => FireonDisConnect(socket));
